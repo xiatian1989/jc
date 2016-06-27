e1c8f1a27ca7a340339df4a005793053f26f9d0b
xiatian1989 <xiahuiwen1989@163.com>
2016-06-27 21:16:43 +0800

    删除不必要的类

--- src/main/java/com/rwkj/jc/dao/CommonDao.java
+++
@@ -1,9 +0,0 @@
-package com.rwkj.jc.dao;
-
-import org.apache.ibatis.annotations.Param;
-
-public interface CommonDao {
-    int createNewTable(@Param("tableName") String tableName);
-    int dropTable(@Param("tableName") String tableName);
-    int existTable(String tableName);
-}
